f4cb678695c56507b896473c165250689e7f21d8
Jonas Severing <jonas.severing@gmail.com>
2021-09-13 21:26:11 +0200

    INIT

---
+++ template.js
@@ -0,0 +1,12 @@
+module.exports = function(scope) {
+  return {
+    package: {
+      dependencies: {
+        'strapi-plugin-graphql': scope.strapiVersion,
+        'strapi-plugin-i18n': scope.strapiVersion,
+        // 'strapi-upload-cloudinary': 'latest',
+        // 'strapi-provider-email-mailgun': 'latest',
+      },
+    },
+  };
+};
